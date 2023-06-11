1349d6df32ede9f0327620b013d626c628810b28
msandulache <marius.sandulache@axonsoft.ro>
2023-06-11 22:43:49 +0300

    more twigs

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -13,4 +13,40 @@ class HomeController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    #[Route('/popular', 'app_popular')]
+    public function popular(): Response
+    {
+        return $this->render('popular.html.twig');
+    }
+
+    #[Route('/top-rated', 'app_top_rated')]
+    public function topRated(): Response
+    {
+        return $this->render('top-rated.html.twig');
+    }
+
+    #[Route('/upcoming', 'app_upcoming')]
+    public function upcoming(): Response
+    {
+        return $this->render('upcoming.html.twig');
+    }
+
+    #[Route('/login', 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('login.html.twig');
+    }
+
+    #[Route('/register', 'app_register')]
+    public function register(): Response
+    {
+        return $this->render('register.html.twig');
+    }
+
+    #[Route('/movie', 'app_movie')]
+    public function movie(): Response
+    {
+        return $this->render('movie.html.twig');
+    }
 }
