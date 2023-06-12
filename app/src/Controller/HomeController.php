25031d1489476bd41cb92adb430d32182a4d91b0
marius sandulache <mariussandulache2015@gmail.com>
2023-06-12 18:29:44 +0300

    new twigs

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -49,4 +49,58 @@ class HomeController extends AbstractController
     {
         return $this->render('movie.html.twig');
     }
+
+    #[Route('/contact', 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
+    #[Route('/ticket', 'app_ticket')]
+    public function ticket(): Response
+    {
+        return $this->render('ticket.html.twig');
+    }
+
+    #[Route('/cart', 'app_cart')]
+    public function cart(): Response
+    {
+        return $this->render('cart.html.twig');
+    }
+
+    #[Route('/checkout', 'app_checkout')]
+    public function checkout(): Response
+    {
+        return $this->render('checkout.html.twig');
+    }
+
+    #[Route('/thank-you', 'app_thank_you')]
+    public function thankYou(): Response
+    {
+        return $this->render('thank-you.html.twig');
+    }
+
+    #[Route('/account', 'app_account')]
+    public function account(): Response
+    {
+        return $this->render('account.html.twig');
+    }
+
+    #[Route('/order-history', 'app_order_history')]
+    public function orderHistory(): Response
+    {
+        return $this->render('order-history.html.twig');
+    }
+
+    #[Route('/password-recovery', 'app_password_recovery')]
+    public function passwordRecovery(): Response
+    {
+        return $this->render('password-recovery.html.twig');
+    }
+
+    #[Route('/ticket-history', 'app_ticket_history')]
+    public function ticketHistory(): Response
+    {
+        return $this->render('ticket-history.html.twig');
+    }
 }
